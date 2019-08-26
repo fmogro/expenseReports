110560c788e4d99487537d915b954dc3e463b6cd
Felipe Mogro <fmogro@eluniverso.com>
2019-08-26 17:27:57 -0500

    Se crean Relaciones

--- app/ExpenseReport.php
+++ app/ExpenseReport.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
+use Expsense;
 use Illuminate\Database\Eloquent\Model;
 
 class ExpenseReport extends Model
 {
-    //
+    public function expenses(){
+        return $this->hasMany(Expense::class);
+    }
 }
